11b9918ed2fb0c1e948e6e9bebded99d81e280a1
hhyyg <hiroka.yago@gmail.com>
2019-03-17 11:23:48 +0900

    Update script

--- publish.sh
+++ publish.sh
@@ -1,3 +1,3 @@
 tsc index.ts
-zip -r ../try-lambda.zip .
+zip ../try-lambda.zip index.js
 aws lambda update-function-code --profile mymiso --function-name myFunctionMiso --zip-file fileb:///Users/hhyyg/Documents/projects/try-lambda.zip --publish

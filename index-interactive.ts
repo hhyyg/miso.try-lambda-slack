c54bc18d99e5288d992c904d2c3292506ca4f21d
hhyyg <hiroka.yago@gmail.com>
2019-03-21 10:44:53 +0900

    Add record to DynamoDB

--- index-interactive.ts
+++ index-interactive.ts
@@ -1,5 +1,11 @@
 import * as querystring from 'querystring';
 import * as axios from 'axios';
+import * as aws from 'aws-sdk';
+import { DynamoDbSettings } from 'aws-sdk/clients/dms';
+
+const dynamo = new aws.DynamoDB.DocumentClient({
+    region: 'ap-northeast-1'
+});
 
 exports.handler = async (event: any) => {
 
@@ -12,6 +18,8 @@ exports.handler = async (event: any) => {
     const userName = payload.user.name;
     const userId = payload.user.id;
     
+    putTicket(userId, userName, request_location_origin, request_location_destination);
+
     const data = {
         text: `Success! userId: ${userId}, userName: ${userName}`,
         response_type: "in_channel",
@@ -32,3 +40,27 @@ exports.handler = async (event: any) => {
 
     return response;
 };
+
+function putTicket(
+        userId: string,
+        userName: string,
+        locOrigin: string,
+        locDestination: string,
+        ) {
+
+    const item: aws.DynamoDB.DocumentClient.PutItemInput = {
+        TableName: 'tickets',
+        Item: {
+            userId,
+            userName,
+            locOrigin,
+            locDestination,
+            createdAt: new Date().toISOString()
+        }
+    };
+    dynamo.put(item, (error, data) => {
+        if (error) {
+            console.error(error);
+        }
+    });
+}
